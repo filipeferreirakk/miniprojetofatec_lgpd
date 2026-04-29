1253fadfd35bef2dc7a64beb8ae020c75422049c
filipeferreirakk <filipegomesfrr@gmail.com>
2026-04-29 00:10:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,21 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date, DateTime, insert, text
+from sqlalchemy import create_engine, text
 from datetime import datetime
 
-import time
-from functools import wraps
-def medir_tempo(func):
-    """Decorator que mede o tempo de execução de uma função."""
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        inicio = time.perf_counter()  # tempo inicial (mais preciso que time.time)
-        resultado = func(*args, **kwargs)
-        fim = time.perf_counter()     # tempo final
-        duracao = fim - inicio
-        print(f"⏱ Função '{func.__name__}' executada em {duracao:.6f} segundos.")
-        return resultado
-    return wrapper
+HOST = '200.19.224.150'
+USER = 'alunos'
+PASSWORD = 'AlunoFatec'
+DATABASE = 'atividade2'
+PORT = '5432'
 
-engine = create_engine("postgresql+psycopg2://alunos:AlunoFatec@200.19.224.150:5432/atividade2", echo=False)
-metadata = MetaData()
+connection_string = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+engine = create_engine(connection_string)
 
-usuarios = Table(
-    'usuarios', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('nome', String(50), nullable=False, index=True),
-    Column('cpf', String(14), nullable=False),
-    Column('email', String(100), nullable=False, unique=True),
-    Column('telefone', String(20), nullable=False),
-    Column('data_nascimento', Date, nullable=False),
-    Column('created_on', DateTime(), default=datetime.now),
-    Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now)
-)
+def testar_conexao():
+    try:
+        with engine.connect() as conn:
+            print("Conexão estabelecida com sucesso!")
+    except Exception as e:
+        print(f"Erro ao conectar: {e}")
 
-metadata.create_all(engine)
-
-@medir_tempo
-def LGPD(row):
-    return row
-
-users = []
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM usuarios LIMIT 5;"))
-    for row in result:
-        row = LGPD(row)
-        users.append(row)
-
-for user in users:
-    print(user)
+if __name__ == "__main__":
+    testar_conexao()
